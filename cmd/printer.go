d26a5beb5bcebfc90827f91890ae9149d0387852
name <email>
2023-05-27 08:01:55 +0000

    minor refactor

--- cmd/printer.go
+++ cmd/printer.go
@@ -17,35 +17,46 @@ func print(prompt string, text string) {
 
 // This is ugly but it works, it needs to be refactored. It also has some bugs in case of out of order directories.
 func visualizeTemplate(template []SingleDir) {
-	// Determine how deep is the path
 	for dirNum, dir := range template {
 		connectorDir := "├──"
 		initString := ""
 		depth := strings.Count(dir.Path, string(os.PathSeparator)) + 1
 		name := strings.Split(dir.Path, string(os.PathSeparator))[depth-1]
-		// Print the path with correct depth
+
 		dirDepth := depth
 		if depth <= 2 {
 			dirDepth = 1
-		}
-		if depth > 2 {
+		} else {
 			initString = "│"
 		}
+
 		if dirNum == len(template)-1 {
 			connectorDir = "└──"
 		}
-		println(initString + strings.Repeat("    ", dirDepth-1) + connectorDir + " 📂 " + color.Ize(color.Blue, name))
-		// Print the files
+
+		printDirectory(initString, dirDepth, connectorDir, name)
+
 		for num, file := range dir.Required.Files {
 			connector := "├──"
 			if depth > 1 {
 				initString = "│"
 			}
+
 			if num == len(dir.Required.Files)-1 {
 				connector = "└──"
 			}
-			println(initString+strings.Repeat("    ", depth-1)+connector+" 💽", color.Ize(color.Green, file))
-		}
 
+			printFile(initString, depth, connector, file)
+		}
 	}
+}
+
+func printDirectory(initString string, dirDepth int, connectorDir string, name string) {
+	output := initString + strings.Repeat("    ", dirDepth-1) + connectorDir + " 📂 " + color.Ize(color.Blue, name)
+	println(output)
+}
+
+func printFile(initString string, depth int, connector string, file string) {
+	output := initString + strings.Repeat("    ", depth-1) + connector + " 💽 " + color.Ize(color.Green, file)
+	println(output)
 }
